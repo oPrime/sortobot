1eb0920322ad8b39ade5ea38722bc5ea28f6cb9a
Mohan Arangan <mohanr2222@gmail.com>
2012-11-02 20:25:51 +0530

    Thor Setup

--- lib/sortobot.rb
+++ lib/sortobot.rb
@@ -1,5 +1,14 @@
-require "sortobot/version"
+require "mime/types"
+require "thor"
+
 
 module Sortobot
-  # Your code goes here...
+	class Cli < Thor
+		desc "push", "push files into directories"
+		def push
+		end
+
+  class Root < Thor  	
+  	register Sortobot::Cli, 'push', 'push', 'Delegates to a sub-command'
+  end
 end
