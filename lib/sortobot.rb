cd61c2abfaaa91ca1db6a303606123744a6432d2
Mohan <mohanr2222@gmail.com>
2012-11-10 20:04:15 +0530

    Fixed desc line

--- lib/sortobot.rb
+++ lib/sortobot.rb
@@ -7,15 +7,13 @@ require "sortobot/helper"
 module Sortobot
 	class Cli < Thor
 		desc "push", "push files into directories"
-		desc "trim", "removes empty directories"
-
 		def push
 			Helper.makedir
 			Helper.move
 			Helper.trim			
 		end
 
-		
+		desc "trim", "removes empty directories"
 		def trim
 			Helper.trim
 		end
